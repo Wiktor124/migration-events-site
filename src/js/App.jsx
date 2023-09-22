c7657c62c0aa318ff9e00dfb53f8179fd1c3c902
JasonPorras <jasonp@isppro.net>
2023-09-21 18:56:19 -0600

    create component card

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,16 @@
 import { createRoot } from "react-dom/client";
+import { GalleryCard } from "./components/Card";
 
 const App = () => {
   return (
     <div>
-      Hola lola!
+        <GalleryCard
+          imageUrl="https://res.cloudinary.com/iireii/image/upload/v1686104576/Cetav/events/music-001_w50seu.jpg"
+          eventName="2023 Dallas Soul Flower Music Fest"
+          date="Saturday, June 17, 2:00 PM"
+          location="Main Street Garden Park • Dallas, TX"
+          price="10.00"
+        />
     </div>
   );
 };
