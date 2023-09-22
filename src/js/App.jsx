427f36bbc70be748d5597d28a4f7fd9a82e90eed
JasonPorras <jasonp@isppro.net>
2023-09-21 21:47:26 -0600

    change in information in the cards

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -13,18 +13,18 @@ const App = () => {
           price="10.00"
         />
         <GalleryCard
-          imageUrl="https://res.cloudinary.com/iireii/image/upload/v1686104576/Cetav/events/music-001_w50seu.jpg"
-          eventName="2023 Dallas Soul Flower Music Fest"
-          date="Saturday, June 17, 2:00 PM"
-          location="Main Street Garden Park • Dallas, TX"
-          price="10.00"
+          imageUrl="https://res.cloudinary.com/iireii/image/upload/v1686105236/Cetav/events/music-002_wex6ub.jpg"
+          eventName="Hookie DC (2023): Cloud 9"
+          date="Friday, June 16, 12:00 PM."
+          location="Festival Grounds • Fort Washington, MD."
+          price="60.00"
         />
         <GalleryCard
-          imageUrl="https://res.cloudinary.com/iireii/image/upload/v1686104576/Cetav/events/music-001_w50seu.jpg"
-          eventName="2023 Dallas Soul Flower Music Fest"
-          date="Saturday, June 17, 2:00 PM"
-          location="Main Street Garden Park • Dallas, TX"
-          price="10.00"
+          imageUrl="https://res.cloudinary.com/iireii/image/upload/v1686105556/Cetav/events/music-004_h768uj.jpg"
+          eventName="Kostume Kult Presents: A.I. A.I. Oh! - Black &amp; Light Ball 2023"
+          date="Saturday, December 2, 5:24 PM."
+          location="The Tailor Public House • New York, NY."
+          price="44.82"
         />
     </ul>
     </>
